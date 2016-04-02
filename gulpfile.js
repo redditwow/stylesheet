e862c448d4e94d11e3914019e8e90fb7aaae20d7
Bryan Channon <bryan@vuii.co.uk>
2016-04-02 23:47:44 +0100

    Initial header style

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 var replace = require('gulp-replace');
 
 gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
+    gulp.src('sass/**/main.scss')
         .pipe(sass().on('error', sass.logError))
         // .pipe( replace('http://b.thumbs.redditmedia.com/JMItoXfQe274DPPZxd_MKmLdjT3vRp-7w8wqcN8mV1Y.png', '%%repeat-bg-media%%') )
         // .pipe( replace('http://b.thumbs.redditmedia.com/kBensLEvjkHoegR5JoeIZ5K3PC5X4jS_AZ9zx_4QBkg.png', '%%repeat-bg%%') )
