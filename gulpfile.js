3585862ffa4f4950a64396482cd0da2d30b28ec2
Bryan Channon <bryan@vuii.co.uk>
2018-07-02 00:36:11 +0100

    New retina thumbnails

--- gulpfile.js
+++ gulpfile.js
@@ -59,8 +59,8 @@ gulp.task('styles', ['sprites', 'sprites-retina', 'flair-link', 'flair-user'], f
 
         // Images that need cache busting
         .pipe(replace('../sprites/spritesheet.png', '%%spritesheet-v9%%'))
-        .pipe(replace('../sprites/spritesheet-retina-1x.png', '%%spritesheet-retina-1x-v1%%'))
-        .pipe(replace('../sprites/spritesheet-retina-2x.png', '%%spritesheet-retina-2x-v1%%'))
+        .pipe(replace('../sprites/spritesheet-retina-1x.png', '%%spritesheet-retina-1x-v2%%'))
+        .pipe(replace('../sprites/spritesheet-retina-2x.png', '%%spritesheet-retina-2x-v2%%'))
         .pipe(replace('../sprites/flair-link.png', '%%flair-link-v3%%'))
         .pipe(replace('../sprites/flair-user.png', '%%flair-user-v8%%'))
 
