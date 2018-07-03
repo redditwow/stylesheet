96e6847f41042f247b1e82a10f7ec4141dbc2988
Bryan Channon <bryan@vuii.co.uk>
2018-07-03 21:29:02 +0100

    Replace old sidebar icons with new flat retina compatible ones. Remove ProCSS from assets. Bump retina spritesheet version

--- gulpfile.js
+++ gulpfile.js
@@ -60,8 +60,8 @@ gulp.task('styles', ['sprites', 'sprites-retina', 'flair-link', 'flair-user'], f
 
         // Images that need cache busting
         .pipe(replace('../sprites/spritesheet.png', '%%spritesheet-v9%%'))
-        .pipe(replace('../sprites/spritesheet-retina-1x.png', '%%spritesheet-retina-1x-v2%%'))
-        .pipe(replace('../sprites/spritesheet-retina-2x.png', '%%spritesheet-retina-2x-v2%%'))
+        .pipe(replace('../sprites/spritesheet-retina-1x.png', '%%spritesheet-retina-1x-v3%%'))
+        .pipe(replace('../sprites/spritesheet-retina-2x.png', '%%spritesheet-retina-2x-v3%%'))
         .pipe(replace('../sprites/flair-link.png', '%%flair-link-v3%%'))
         .pipe(replace('../sprites/flair-user.png', '%%flair-user-v8%%'))
 
