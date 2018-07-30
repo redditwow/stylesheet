5a6c7bcba54924e58607cc26320c8da9abe9f41a
Bryan Channon <bryan@vuii.co.uk>
2018-07-30 20:48:15 +0100

    A silly meme

--- gulpfile.js
+++ gulpfile.js
@@ -73,8 +73,8 @@ gulp.task('styles', ['sprites-retina', 'sprites-snoo', 'sprites-logo', 'flair-us
         .pipe(replace('../sprites/flair-user-1x.png', '%%flair-user-1x-v11%%'))
         .pipe(replace('../sprites/flair-user-2x.png', '%%flair-user-2x-v11%%'))
 
-        .pipe(replace('../sprites/flair-user-bespoke-1x.png', '%%flair-user-bespoke-1x-v4%%'))
-        .pipe(replace('../sprites/flair-user-bespoke-2x.png', '%%flair-user-bespoke-2x-v4%%'))
+        .pipe(replace('../sprites/flair-user-bespoke-1x.png', '%%flair-user-bespoke-1x-v7%%'))
+        .pipe(replace('../sprites/flair-user-bespoke-2x.png', '%%flair-user-bespoke-2x-v7%%'))
 
         .pipe(replace('{{DEV}}', devMessage))
         // reddit doesn't like @charset, so just remove it...
