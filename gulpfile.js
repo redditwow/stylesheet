96e1ea3b5c0ec46a2b7a86aee2bce967085cbfb3
Bryan Channon <bryan@vuii.co.uk>
2018-12-28 15:37:54 +0000

    Replace the xmas snoos with regular snoos

--- gulpfile.js
+++ gulpfile.js
@@ -70,8 +70,8 @@ gulp.task('styles', ['sprites-retina', 'sprites-snoo', 'sprites-logo', 'flair-us
         .pipe(replace('../sprites/spritesheet-retina-1x.png', '%%spritesheet-retina-1x-v19%%'))
         .pipe(replace('../sprites/spritesheet-retina-2x.png', '%%spritesheet-retina-2x-v19%%'))
 
-        .pipe(replace('../sprites/spritesheet-snoo-1x.png', '%%spritesheet-snoo-1x-v3%%'))
-        .pipe(replace('../sprites/spritesheet-snoo-2x.png', '%%spritesheet-snoo-2x-v3%%'))
+        .pipe(replace('../sprites/spritesheet-snoo-1x.png', '%%spritesheet-snoo-1x-v4%%'))
+        .pipe(replace('../sprites/spritesheet-snoo-2x.png', '%%spritesheet-snoo-2x-v4%%'))
 
         .pipe(replace('../sprites/spritesheet-logo-1x.png', '%%spritesheet-logo-1x-v0%%'))
         .pipe(replace('../sprites/spritesheet-logo-2x.png', '%%spritesheet-logo-2x-v0%%'))
