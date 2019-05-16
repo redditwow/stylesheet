4581677664b305d884a732b93947b17c5877d3d0
Bryan Channon <bryan@vuii.co.uk>
2019-05-16 22:31:59 +0100

    New flairs

--- gulpfile.js
+++ gulpfile.js
@@ -90,8 +90,8 @@ gulp.task('styles', ['sprites-retina', 'sprites-snoo', 'sprites-logo', 'flair-us
         .pipe(replace('../sprites/flair-user-1x.png', '%%flair-user-1x-v11%%'))
         .pipe(replace('../sprites/flair-user-2x.png', '%%flair-user-2x-v11%%'))
 
-        .pipe(replace('../sprites/flair-user-bespoke-1x.png', '%%flair-user-bespoke-1x-v13%%'))
-        .pipe(replace('../sprites/flair-user-bespoke-2x.png', '%%flair-user-bespoke-2x-v13%%'))
+        .pipe(replace('../sprites/flair-user-bespoke-1x.png', '%%flair-user-bespoke-1x-v14%%'))
+        .pipe(replace('../sprites/flair-user-bespoke-2x.png', '%%flair-user-bespoke-2x-v14%%'))
 
         .pipe(replace('{{DEV}}', devMessage))
         // reddit doesn't like @charset, so just remove it...
