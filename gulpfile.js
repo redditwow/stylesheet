db3dc96d687b9a5c55defc884257f2c22462492d
Bryan Channon <bryan@vuii.co.uk>
2020-12-07 18:45:14 +0000

    It's not Christmas til the jailer gets a santa hat.

--- gulpfile.js
+++ gulpfile.js
@@ -97,8 +97,8 @@ gulp.task('styles', ['sprites-retina', 'sprites-snoo', 'sprites-logo', 'flair-us
         .pipe(replace('../sprites/spritesheet-retina-1x.png', '%%spritesheet-retina-1x-v26%%'))
         .pipe(replace('../sprites/spritesheet-retina-2x.png', '%%spritesheet-retina-2x-v26%%'))
 
-        .pipe(replace('../sprites/spritesheet-snoo-1x.png', '%%spritesheet-snoo-1x-v5%%'))
-        .pipe(replace('../sprites/spritesheet-snoo-2x.png', '%%spritesheet-snoo-2x-v5%%'))
+        .pipe(replace('../sprites/spritesheet-snoo-1x.png', '%%spritesheet-snoo-xmas-1x%%'))
+        .pipe(replace('../sprites/spritesheet-snoo-2x.png', '%%spritesheet-snoo-xmas-2x%%'))
 
         .pipe(replace('../sprites/spritesheet-logo-1x.png', '%%spritesheet-logo-1x-v1%%'))
         .pipe(replace('../sprites/spritesheet-logo-2x.png', '%%spritesheet-logo-2x-v1%%'))
