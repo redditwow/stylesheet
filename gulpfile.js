f8ff48d016482793df4e93f657f90224cd29c95b
Bryan Channon <bryan@vuii.co.uk>
2021-07-29 13:49:15 +0100

    Blue heart flair

--- gulpfile.js
+++ gulpfile.js
@@ -108,8 +108,8 @@ gulp.task('styles', ['sprites-retina', 'sprites-snoo', 'sprites-logo', 'flair-us
         .pipe(replace('../sprites/flair-user-1x.png', '%%flair-user-1x-v16%%'))
         .pipe(replace('../sprites/flair-user-2x.png', '%%flair-user-2x-v16%%'))
 
-        .pipe(replace('../sprites/flair-user-bespoke-1x.png', '%%flair-user-bespoke-1x-v28%%'))
-        .pipe(replace('../sprites/flair-user-bespoke-2x.png', '%%flair-user-bespoke-2x-v28%%'))
+        .pipe(replace('../sprites/flair-user-bespoke-1x.png', '%%flair-user-bespoke-1x-v29%%'))
+        .pipe(replace('../sprites/flair-user-bespoke-2x.png', '%%flair-user-bespoke-2x-v29%%'))
 
         .pipe(replace('{{DEV}}', devMessage))
         // reddit doesn't like @charset, so just remove it...
