aaa86209a7b30c42f0ae34e871fb35d4eac6f15e
Bryan Channon <bryan@vuii.co.uk>
2022-02-16 22:59:21 +0000

    Update for latest node

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 var replace = require('gulp-replace');
 var spritesmith = require('gulp.spritesmith');
 var cleanCSS = require('gulp-clean-css');
