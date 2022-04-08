412fedbd86ef978518fdbf9a4d53e6ec30c1c4ea
Bryan Channon <bryan@vuii.co.uk>
2022-04-08 20:01:51 +0100

    Add echo flair

--- gulpfile.js
+++ gulpfile.js
@@ -96,8 +96,8 @@ gulp.task('styles', ['sprites-retina', 'sprites-snoo', 'sprites-logo', 'flair-us
 
         // Reddit aggressively caches images, so all of these images have a version number on the end to bust the cache
         // after changes have been made. This is a manual process :(
-        .pipe(replace('../sprites/spritesheet-retina-1x.png', '%%spritesheet-retina-1x-v27%%'))
-        .pipe(replace('../sprites/spritesheet-retina-2x.png', '%%spritesheet-retina-2x-v27%%'))
+        .pipe(replace('../sprites/spritesheet-retina-1x.png', '%%spritesheet-retina-1x-v28%%'))
+        .pipe(replace('../sprites/spritesheet-retina-2x.png', '%%spritesheet-retina-2x-v28%%'))
 
         .pipe(replace('../sprites/spritesheet-snoo-1x.png', '%%spritesheet-snoo-1x-v5%%'))
         .pipe(replace('../sprites/spritesheet-snoo-2x.png', '%%spritesheet-snoo-2x-v5%%'))
@@ -105,11 +105,11 @@ gulp.task('styles', ['sprites-retina', 'sprites-snoo', 'sprites-logo', 'flair-us
         .pipe(replace('../sprites/spritesheet-logo-1x.png', '%%spritesheet-logo-1x-v1%%'))
         .pipe(replace('../sprites/spritesheet-logo-2x.png', '%%spritesheet-logo-2x-v1%%'))
 
-        .pipe(replace('../sprites/flair-user-1x.png', '%%flair-user-1x-v16%%'))
-        .pipe(replace('../sprites/flair-user-2x.png', '%%flair-user-2x-v16%%'))
+        .pipe(replace('../sprites/flair-user-1x.png', '%%flair-user-1x-v17%%'))
+        .pipe(replace('../sprites/flair-user-2x.png', '%%flair-user-2x-v17%%'))
 
-        .pipe(replace('../sprites/flair-user-bespoke-1x.png', '%%flair-user-bespoke-1x-v29%%'))
-        .pipe(replace('../sprites/flair-user-bespoke-2x.png', '%%flair-user-bespoke-2x-v29%%'))
+        .pipe(replace('../sprites/flair-user-bespoke-1x.png', '%%flair-user-bespoke-1x-v32%%'))
+        .pipe(replace('../sprites/flair-user-bespoke-2x.png', '%%flair-user-bespoke-2x-v32%%'))
 
         .pipe(replace('{{DEV}}', devMessage))
         // reddit doesn't like @charset, so just remove it...
